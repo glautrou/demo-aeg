175d8b2383c1d8c3254955b87fedc25703c79205
Gilles Lautrou <glautrou@webnet.fr>
2023-06-01 11:30:18 +0200

    Ajout logs et vérification sécurité

--- DemoAeg/DemoAeg.Api/Program.cs
+++ DemoAeg/DemoAeg.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Azure.EventGrid.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -19,6 +21,35 @@ app.UseHttpsRedirection();
 app.MapGet("/", () =>
 {
     return "OK";
+})
+.WithOpenApi();
+
+const string HEADER_SUBSCRIPTION_NAME = "aeg-subscription-name";
+const string SUBSCRIPTION_VALUE = "DEMO-API";
+const string HEADER_SECRET = "demo-secret";
+const string SECRET_VALUE = "123456";
+
+app.MapPost("/webhook/", (EventGridEvent[] events, HttpRequest request, ILogger logger) =>
+{
+    //Log
+    var deliveryCount = request.Headers["aeg-delivery-count"].FirstOrDefault() + 1;
+    logger.LogInformation($"Event recu - Type : {events.FirstOrDefault()?.EventType} (tentative {deliveryCount})");
+
+    //Security
+    var subscriptionName = request.Headers[HEADER_SUBSCRIPTION_NAME].FirstOrDefault();
+    if (subscriptionName != SUBSCRIPTION_VALUE)
+    {
+        logger.LogWarning($"Appel webhook forbidden. Nom de souscription incorrect : {subscriptionName}");
+        return StatusCodes.Status403Forbidden;
+    }
+    var secret = request.Headers[HEADER_SECRET].FirstOrDefault();
+    if (secret != SECRET_VALUE)
+    {
+        logger.LogWarning($"Appel webhook forbidden. Secret incorrect : {secret}");
+        return StatusCodes.Status403Forbidden;
+    }
+
+    return StatusCodes.Status200OK;
 })
 .WithOpenApi();
 
