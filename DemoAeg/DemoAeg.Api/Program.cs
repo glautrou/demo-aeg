b958e56df486c01da7414efa7a4f014d26f15c81
Gilles Lautrou <glautrou@webnet.fr>
2023-06-01 16:49:10 +0200

    Finalisation API en réception

--- DemoAeg/DemoAeg.Api/Program.cs
+++ DemoAeg/DemoAeg.Api/Program.cs
@@ -1,4 +1,7 @@
-using Microsoft.Azure.EventGrid.Models;
+using Azure.Messaging.EventGrid;
+using Azure.Messaging.EventGrid.SystemEvents;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,39 +22,92 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/", () =>
-{
-    return "OK";
-})
-.WithOpenApi();
-
 const string HEADER_SUBSCRIPTION_NAME = "aeg-subscription-name";
-const string SUBSCRIPTION_VALUE = "DEMO-API";
-const string HEADER_SECRET = "demo-secret";
+const string SUBSCRIPTION_VALUE = "egt-demo-aeg-es-api";
+const string HEADER_SECRET = "demo-aeg-secret";
 const string SECRET_VALUE = "123456";
+const string EVENTTYPE_TELEPHONIE_DECROCHER = "Demo.Telephonie.Decrocher";
 
 app.MapPost("/webhook/", (EventGridEvent[] events, HttpRequest request, ILogger<Program> logger) =>
 {
-    //Log
+    #region Log
     var deliveryCount = request.Headers["aeg-delivery-count"].FirstOrDefault() + 1;
     logger.LogInformation($"Event recu - Type : {events.FirstOrDefault()?.EventType} (tentative {deliveryCount})");
+    #endregion Log
 
-    //Security
+    #region Security
     var subscriptionName = request.Headers[HEADER_SUBSCRIPTION_NAME].FirstOrDefault();
-    if (subscriptionName != SUBSCRIPTION_VALUE)
+    if (subscriptionName?.ToUpperInvariant() != SUBSCRIPTION_VALUE.ToUpperInvariant())
     {
         logger.LogWarning($"Appel webhook forbidden. Nom de souscription incorrect : {subscriptionName}");
-        return StatusCodes.Status403Forbidden;
+        return Results.Forbid();
     }
     var secret = request.Headers[HEADER_SECRET].FirstOrDefault();
     if (secret != SECRET_VALUE)
     {
         logger.LogWarning($"Appel webhook forbidden. Secret incorrect : {secret}");
-        return StatusCodes.Status403Forbidden;
+        return Results.Forbid();
     }
+    #endregion Security
 
-    return StatusCodes.Status200OK;
+    #region Processing
+    foreach (var evt in events)
+    {
+        if (evt.TryGetSystemEventData(out var systemEvent))
+        {
+            #region Processing > System events
+            //System event
+            switch (systemEvent)
+            {
+                case SubscriptionValidationEventData subscriptionValidated:
+                    //Requis pour enregistrement Webhook : https://docs.microsoft.com/en-us/azure/event-grid/webhook-event-delivery
+                    var response = new SubscriptionValidationResponse
+                    {
+                        ValidationResponse = subscriptionValidated.ValidationCode
+                    };
+                    return Results.Ok(response);
+                default:
+                    var errorMessage = $"Event système non géré : {evt.EventType}";
+                    logger.LogWarning(errorMessage, evt.Data.ToString());
+                    return Results.UnprocessableEntity();
+            }
+            #endregion Processing > System events
+        }
+        else
+        {
+            #region Processing > Custom events
+            //Désérialisation
+            var serializer = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                ReferenceHandler = ReferenceHandler.Preserve
+            };
+            var eventData = evt.Data.ToObjectFromJson<TelephonieEventData>(serializer);
+
+            switch (evt.EventType)
+            {
+                case EVENTTYPE_TELEPHONIE_DECROCHER:
+                    logger.LogInformation($"Event={EVENTTYPE_TELEPHONIE_DECROCHER}, Destinataire={eventData.AgentLogin}, Appelant={eventData.CustNumber}");
+                    return Results.Ok();
+                default:
+                    var errorMessage = $"Event custom non géré : {evt.EventType}";
+                    logger.LogWarning(errorMessage, evt.Data.ToString());
+                    return Results.UnprocessableEntity();
+            }
+            #endregion Processing > Custom events
+        }
+    }
+    #endregion Processing
+
+    return Results.UnprocessableEntity();
 })
 .WithOpenApi();
 
 app.Run();
+
+class TelephonieEventData
+{
+    public string AgentLogin { get; set; }
+    public string CustNumber { get; set; }
+    public string WaitDuration { get; set; }
+}
